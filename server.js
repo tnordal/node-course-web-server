b9a3052fd51a70757188fb725b102fd092513081
Tom Nordal <tnordal@gmail.com>
2018-10-20 18:02:52 +0200

    Added project page

--- server.js
+++ server.js
@@ -64,6 +64,13 @@ app.get('/bad', (req, res) => {
     });
 });
 
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'Projects Page',
+        welcomeMessage: 'List of projects:'
+    });
+});
+
 app.listen(port, () => {
     console.log(`Server is up on port ${port}`);
 });
